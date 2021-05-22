c9761f2b5cec297e799d1a23a7aadd4bfe4fbf9b
Shubhanu sharma <shubhanus@live.com>
2021-05-22 11:48:30 +0530

    feat(categories): added categries section data feat(newReleases): added new releases section data feat(featuredPlaylist): added featured playlist section data

--- spootify/src/config.js
+++ spootify/src/config.js
@@ -1,8 +1,8 @@
 export default {
   api: {
-    baseUrl: 'https://api.spotify.com/v1',
-    authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
-  }
-}
+    baseUrl: "https://api.spotify.com/v1",
+    authUrl: "https://accounts.spotify.com/api/token",
+    clientId: "d72b3138d722437182e0964f8bfc44c7",
+    clientSecret: "1b315f9cc57b415fb0a7901d6fc0caeb",
+  },
+};
--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
-import '../styles/_discover.scss';
+import React, { Component } from "react";
+import DiscoverBlock from "./DiscoverBlock/components/DiscoverBlock";
+import "../styles/_discover.scss";
+import actions from "../../../actions";
 
 export default class Discover extends Component {
   constructor() {
@@ -9,18 +10,61 @@ export default class Discover extends Component {
     this.state = {
       newReleases: [],
       playlists: [],
-      categories: []
+      categories: [],
     };
   }
+
+  componentDidMount = async () => {
+    // requirements are not clear so going ahead with best UX i can think of
+    // In case if client wants to fetch all in one shot then show data
+    // then we can use promise.all that will send parallel req
+    await this.getDiscoverDataAndSetState(
+      "newReleases",
+      actions.getNewReleases
+    );
+    await this.getDiscoverDataAndSetState(
+      "playlists",
+      actions.getFeaturedPlaylists
+    );
+    await this.getDiscoverDataAndSetState("categories", actions.getCategories);
+  };
+
+  getDiscoverDataAndSetState = (stateType, api, time) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        api().then((data) => {
+          this.setState(
+            {
+              [stateType]: data,
+            },
+            resolve
+          );
+        });
+      }, time);
+    });
+  };
 
   render() {
     const { newReleases, playlists, categories } = this.state;
 
     return (
       <div className="discover">
-        <DiscoverBlock text="RELEASED THIS WEEK" id="released" data={newReleases} />
-        <DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
-        <DiscoverBlock text="BROWSE" id="browse" data={categories} imagesKey="icons" />
+        <DiscoverBlock
+          text="RELEASED THIS WEEK"
+          id="released"
+          data={newReleases}
+        />
+        <DiscoverBlock
+          text="FEATURED PLAYLISTS"
+          id="featured"
+          data={playlists}
+        />
+        <DiscoverBlock
+          text="BROWSE"
+          id="browse"
+          data={categories}
+          imagesKey="icons"
+        />
       </div>
     );
   }
